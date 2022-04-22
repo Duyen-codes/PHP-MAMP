8a62b1d408cbcaf1225a03ce58a358b78fbf2345
Duyen Nguyen <hongduyen0705@gmail.com>
2022-04-22 11:25:36 +0300

    cleaned spaces in 2.php

--- phpDir/src/PHP_practice01/2.php
+++ phpDir/src/PHP_practice01/2.php
@@ -2,33 +2,16 @@
 <?php include "includes/header.php"; ?>
 
 <section class="content">
-
 	<aside class="col-xs-4">
-
 		<?php Navigation(); ?>
-
-
 	</aside>
 	<!--SIDEBAR-->
-
-
 	<article class="main-content col-xs-8">
-
-
 		<?php
-
-
-
 		/* Step 1: Make 2 variables called number1 and number2 and set 1 to value 10 and the other 20:
-
 		  Step 2: Add the two variables and display the sum with echo:
-
-
 		  Step3: Make 2 Arrays with the same values, one regular and the other associative
-
-		 
 			 */
-
 		?>
 		<?php 
 		$number1 = 10;
@@ -36,7 +19,6 @@
 		$sum = $number1 + $number2;
 		?>
 		<?= $sum ?>
-
 		<?php 
 		// echo $number + $number2;
 		?>
