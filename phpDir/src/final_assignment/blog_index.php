1f9e5cf1d5bfd11e26a94215f27da7b885b8167e
Duyen Nguyen <hongduyen0705@gmail.com>
2022-04-23 09:09:05 +0300

    set up snap project

--- phpDir/src/final_assignment/blog_index.php
+++ phpDir/src/final_assignment/blog_index.php
@@ -8,6 +8,14 @@
             echo "<h3>Hello " . $_SESSION['useruid'] . "." . " Welcome back!" . "</h3>"; 
         }
       ?>
+    
     </section>
+    <section>
+      <h2>We are coming soon. </h2>
+    <form action="blog_index.php" method="post">
+        <input type="text" placeholder="email" name="email">
+        <input type="submit" name="submit" value="Submit">
+      </form>
+      </section>
 <?php 
 include_once 'blog_footer.php';
