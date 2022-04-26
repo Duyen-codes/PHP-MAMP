7f19cbbc1e35f2433ccef976c7ff3daf4991092e
Duyen Nguyen <hongduyen0705@gmail.com>
2022-04-26 22:10:33 +0300

    check form submit, grab filename and content, created files directory

--- phpDir/src/snap_introPage/blog.php
+++ phpDir/src/snap_introPage/blog.php
@@ -1,9 +1,18 @@
 <?php include 'header.php'; ?>
+<?php
+$dir = __DIR__ . '/files';
+if (($_SERVER['REQUEST_METHOD'] == 'POST') && (isset($_POST['save-file']))) {
+    $filename = isset($_POST['file']) ? $_POST['file'] : null;
+    $content = isset($_POST['content']) ? $_POST['content'] : null;
+    echo $filename;
+    echo $content;
+}
+?>
 <section>
     <h2>Blog</h2>
-    <form action="">
+    <form action="" method="post">
         <input type="text" name="file">
         <textarea name="content" id="" cols="30" rows="10"></textarea><br>
-        <button type="submit" name="submit">Post</button>
+        <button type="submit" name="save-file">Post</button>
     </form>
 </section>
